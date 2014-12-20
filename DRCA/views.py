83b6d7923f46af356240013d5ae399fae90854e4
João Paulo Clarindo <jpaulo.cpm@gmail.com>
2014-12-20 00:34:35 -0300

    Adicionando a view de matricula

--- DRCA/views.py
+++ DRCA/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django_tables2 import RequestConfig
 from DRCA.tables import *
+from DRCA.models import *
 
 
 def index(request):
@@ -18,3 +19,26 @@ def aluno(request, aluno_id):
     table = AlunoTable(Aluno.objects.all())
     RequestConfig(request).configure(table)
     return render(request, 'alunos.html', {'table': table})
+
+def matricula(request):
+    aluno = Aluno.objects.get(id=0)
+    curso = Curso.objects.get(cod_curso = aluno.cod_curso)
+
+    if curso.tipo_curso == "Graduação":
+        matricular = "ECOM1622"
+
+        disciplina = Disciplina.objects.get(cod_disciplina = matricular)
+        disciplinasCursadas = Disciplinas_cursadas.objects.get(matricula_aluno_id=aluno.matricula_aluno,cod_disciplina_id=matricular)
+
+        #Iniciando matricula
+        if not disciplina.e_oferecida:
+            print("A disciplina não está disponível para matrícula (não oferecida)")
+
+        elif not (aluno.creditos_obrigatorios + aluno.creditos_eletivas) >= disciplina.requisito_credito:
+            print("A disciplina não está disponível para matrícula (créditos insuficientes)")
+
+        elif disciplinasCursadas is None:
+            print("A disciplina não está disponível para matrícula (disciplina cursada)")
+
+        else:
+            Disciplinas_cursadas.create(cod_disciplina_id=matricular, matricula_aluno_id=aluno.matricula_aluno)
